d641477f06dbb1242f02efaa82e10945951485b7
jpzs444 <75724780+jpzs444@users.noreply.github.com>
2024-01-03 18:31:53 +0800

    refactor: Create DrumPad instances/components by using map() method

--- src/components/DrumMachine.jsx
+++ src/components/DrumMachine.jsx
@@ -1,54 +1,18 @@
+import { drumPads } from "../constants"
 import DrumPad from "./DrumPad"
 
 const DrumMachine = () => {
   return (
     <div id="drum-machine">
       <div className="drumpads">
-        <DrumPad 
-          text="Q"
-          keyCode={81} 
-          audioDesc="Heater-1" 
-          audioSample="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"/>
-        <DrumPad 
-          text="W"
-          keyCode={87} 
-          audioDesc="Heater-2" 
-          audioSample="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"/>
-        <DrumPad 
-          text="E"
-          keyCode={69} 
-          audioDesc="Heater-3" 
-          audioSample="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"/>
-        <DrumPad 
-          text="A"
-          keyCode={65} 
-          audioDesc="Heater-4" 
-          audioSample="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"/>
-        <DrumPad 
-          text="S"
-          keyCode={83} 
-          audioDesc="Clap" 
-          audioSample="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"/>
-        <DrumPad 
-          text="D"
-          keyCode={68} 
-          audioDesc="Open-HH" 
-          audioSample="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"/>
-        <DrumPad 
-          text="Z"
-          keyCode={90} 
-          audioDesc="Kick-n'-Hat" 
-          audioSample="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"/>
-        <DrumPad 
-          text="X"
-          keyCode={88} 
-          audioDesc="Kick" 
-          audioSample="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"/>     
-        <DrumPad 
-          text="C"
-          keyCode={67} 
-          audioDesc="Closed-HH" 
-          audioSample="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"/>  
+        {drumPads.map((drumPad, ind) => (
+          <DrumPad
+            key={ind}
+            text={drumPad.text}
+            keyCode={drumPad.keyCode}
+            audioDesc={drumPad.audioDesc}
+            audioSample={drumPad.audioSample}/>
+        ))}
       </div>
 
       <p id="display">Display</p>
---
+++ src/constants/index.js
@@ -0,0 +1,56 @@
+export const drumPads = [
+  {
+    text: "Q",
+    keyCode: 81,
+    audioDesc: "Heater-1",
+    audioSample: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  },
+  {
+    text: "W",
+    keyCode: 87,
+    audioDesc: "Heater-2",
+    audioSample: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+  },
+  {
+    text: "E",
+    keyCode: 69,
+    audioDesc: "Heater-3",
+    audioSample: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+  },
+  {
+    text: "A",
+    keyCode: 65,
+    audioDesc: "Heater-4",
+    audioSample: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+  },
+  {
+    text: "S",
+    keyCode: 83,
+    audioDesc: "Clap",
+    audioSample: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  },
+  {
+    text: "D",
+    keyCode: 68,
+    audioDesc: "Open-HH",
+    audioSample: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+  },
+  {
+    text: "Z",
+    keyCode: 90,
+    audioDesc: "Kick-n'-Hat",
+    audioSample: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  },
+  {
+    text: "X",
+    keyCode: 88,
+    audioDesc: "Kick",
+    audioSample: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  },
+  {
+    text: "C",
+    keyCode: 67,
+    audioDesc: "Closed-HH",
+    audioSample: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+  }
+]
